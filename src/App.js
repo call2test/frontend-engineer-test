b92e920ae4488f2415ffeafb891661e493aabd3b
Jay Renteria <jay.renteria@call2test.com>
2020-03-02 17:32:54 -0700

    ADDS: logo and favicon, start to test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './r7t-iso.svg';
 import './App.css';
 
 function App() {
@@ -7,17 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Welcome to the Reconnect React Test</p>
       </header>
     </div>
   );
